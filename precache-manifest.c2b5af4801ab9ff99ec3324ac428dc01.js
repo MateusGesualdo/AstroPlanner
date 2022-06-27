7c236f8312278c4ddc4d16bb2cbef2bb62364b73
MateusGesualdo <57274184+MateusGesualdo@users.noreply.github.com>
2022-06-27 12:42:15 -0300

    Updates

---
+++ precache-manifest.c2b5af4801ab9ff99ec3324ac428dc01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f75029478d62e8aa94a7b9a8bda27ec7",
+    "url": "/AstroPlanner/index.html"
+  },
+  {
+    "revision": "580fb0380b6795bb0965",
+    "url": "/AstroPlanner/static/js/2.736a9be6.chunk.js"
+  },
+  {
+    "revision": "95d03bc3401287b27141",
+    "url": "/AstroPlanner/static/js/main.3a796916.chunk.js"
+  },
+  {
+    "revision": "69d599f1dd117c6373e6",
+    "url": "/AstroPlanner/static/js/runtime~main.4a6e915b.js"
+  },
+  {
+    "revision": "45d78fdb069d556382dadf75664383e0",
+    "url": "/AstroPlanner/static/media/background.45d78fdb.jpg"
+  }
+]);
